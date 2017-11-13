b90b7d4eaa0fe01ad90665a136dca47926d61e0c
蔡孝玠 <asky.hunsin@gmail.com>
2017-11-13 13:24:06 +0800

    Switch API url, no need of transform pacakges

--- src/twse/isin.go
+++ src/twse/isin.go
@@ -4,15 +4,11 @@ import (
 	"bytes"
 	"fmt"
 	"io/ioutil"
-	"net/http"
 	"regexp"
 	"time"
-
-	"golang.org/x/text/encoding/traditionalchinese"
-	"golang.org/x/text/transform"
 )
 
-const urlISIN = "http://isin.twse.com.tw/isin/e_single_main.jsp?owncode=%s"
+const urlISIN = "http://isin.twse.com.tw/isin/e_class_main.jsp?owncode=%s&market=1"
 
 var (
 	reg      = regexp.MustCompile(">.*</td>")
@@ -21,27 +17,21 @@ var (
 	iPermit  = make(chan bool)
 )
 
-// conv converts the Big5-encodeg data from given url to UTF-8
-func conv(url string) ([]byte, error) {
+// parseISIN extracts the Security from urlISIN with given code
+func parseISIN(code string) (*Stock, error) {
 	<-iPermit
-	go func() {
+	defer func() {
 		time.Sleep(80 * time.Millisecond) // release after 0.08s
-		iPermit <- true
+		go func() { iPermit <- true }()
 	}()
 
-	res, err := http.Get(url)
+	res, err := client.Get(fmt.Sprintf(urlISIN, code))
 	if err != nil {
 		return nil, err
 	}
 	defer res.Body.Close()
 
-	src := transform.NewReader(res.Body, traditionalchinese.Big5.NewDecoder())
-	return ioutil.ReadAll(src)
-}
-
-// parseISIN extracts the Security from urlISIN with given code
-func parseISIN(code string) (*Stock, error) {
-	out, err := conv(fmt.Sprintf(urlISIN, code))
+	out, err := ioutil.ReadAll(res.Body)
 	if err != nil {
 		return nil, err
 	}
