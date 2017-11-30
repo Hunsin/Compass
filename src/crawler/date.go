be4f226dd054e0e140041d3eb9b87a86aab66a0f
Hunsin <asky.hunsin@gmail.com>
2017-12-01 01:04:19 +0800

    crawler.Date full functions

--- src/crawler/date.go
+++ src/crawler/date.go
@@ -1,6 +1,9 @@
 package crawler
 
-import "time"
+import (
+	"fmt"
+	"time"
+)
 
 // A Date specifies the year, month and day.
 type Date struct {
@@ -34,4 +37,47 @@ func (d Date) After(t Date) bool {
 // Before reports whether d is before t.
 func (d Date) Before(t Date) bool {
 	return t.After(d)
+}
+
+// Equal reports whether d and t are the same.
+func (d Date) Equal(t Date) bool {
+	return !d.After(t) && !d.Before(t)
+}
+
+// MarshalJSON implements the json.Marshaler interface.
+// The output is in format as "2006/01/02".
+func (d Date) MarshalJSON() ([]byte, error) {
+	s := d.String()
+	b := make([]byte, 0, len(s)+2)
+	b = append(b, '"')
+	b = append(b, []byte(s)...)
+	return append(b, '"'), nil
+}
+
+// String returns a string of date in format "2006/01/02"
+func (d Date) String() string {
+	return fmt.Sprintf("%4d/%02d/%02d", d.Year, d.Month, d.Day)
+}
+
+// UnmarshalJSON implements the json.Unmarshaler interface.
+// It supports the formats as "2006/01/02", "2006-01-02" or "02 Jan 2006".
+func (d *Date) UnmarshalJSON(b []byte) error {
+	var t time.Time
+	var err error
+
+	s := string(b)[1 : len(b)-1]
+	for _, l := range []string{"2006/01/02", "2006-01-02", "02 Jan 2006"} {
+		t, err = time.Parse(l, s)
+		if err == nil {
+			break
+		}
+	}
+	if err != nil {
+		return err
+	}
+
+	d.Year = t.Year()
+	d.Month = t.Month()
+	d.Day = t.Day()
+	return nil
 }
--- src/crawler/date_test.go
+++ src/crawler/date_test.go
@@ -6,24 +6,96 @@ import (
 )
 
 var (
-	d1 = Date{2010, time.March, 10}
-	d2 = Date{2017, time.November, 30}
+	d1 = Date{2001, time.March, 5}
+	d2 = Date{2009, time.November, 15}
 )
+
+func TestParseDate(t *testing.T) {
+	layouts := map[string]string{
+		"2006/01/02": "2001/03/05",
+		"2006-1-02":  "2001-3-05",
+		"Jan 2 2006": "Mar 05 2001",
+	}
+	for l, s := range layouts {
+		d, err := ParseDate(l, s)
+		if err != nil {
+			t.Errorf("ParseDate exit with error: %v", err)
+		}
+		if d.Year != d1.Year ||
+			d.Month != d1.Month ||
+			d.Day != d1.Day {
+			t.Errorf("ParseDate failed: parse %s returns %v", d)
+		}
+	}
+
+	// Invalid date should return error
+	_, err := ParseDate("2006/01/02", "2017/13/01")
+	if err == nil {
+		t.Error("ParseDate failed: Invalid input doesn't return error")
+	}
+}
 
 func TestAfter(t *testing.T) {
 	if d1.After(d2) {
-		t.Error("d1 should not after d2")
+		t.Error("Date.After failed: d1 should not after d2")
 	}
 	if d1.After(d1) {
-		t.Error("d1 should not after d1")
+		t.Error("Date.After failed: d1 should not after d1")
 	}
 }
 
 func TestBefore(t *testing.T) {
 	if d2.Before(d1) {
-		t.Error("d2 should not before d1")
+		t.Error("Date.Before failed: d2 should not before d1")
 	}
 	if d2.After(d2) {
-		t.Error("d2 should not after d2")
+		t.Error("Date.Before failed: d2 should not after d2")
+	}
+}
+
+func TestEqual(t *testing.T) {
+	if d1.Equal(d2) {
+		t.Error("Date.Equal failed: d1 != d2")
+	}
+	if !d2.Equal(d2) {
+		t.Error("Date.Equal failed: d2 == d2")
+	}
+}
+
+func TestMarshalJSON(t *testing.T) {
+	b, err := d1.MarshalJSON()
+	if err != nil {
+		t.Errorf("Date.MarshalJSON exits with error: %v", err)
+	}
+
+	w := `"2001/03/05"`
+	if string(b) != w {
+		t.Errorf("Date.MarshalJSON failed. want: %s, got: %s", w, string(b))
+	}
+}
+
+func TestString(t *testing.T) {
+	w := "2009/11/15"
+	if d2.String() != w {
+		t.Errorf("Date.String failed. want: %s, got: %v", w, d2)
+	}
+}
+
+func TestUnmarshalJSON(t *testing.T) {
+	bs := [][]byte{
+		[]byte(`"2009-11-15"`),
+		[]byte(`"2009/11/15"`),
+		[]byte(`"15 Nov 2009"`),
+	}
+
+	var d Date
+	for _, b := range bs {
+		err := d.UnmarshalJSON(b)
+		if err != nil {
+			t.Errorf("Date.UnmarshalJSON exits with error: %v", err)
+		}
+		if !d.Equal(d2) {
+			t.Errorf("Date.UnmarshalJSON failed. want: %v, got: %v", d2, d)
+		}
 	}
 }
