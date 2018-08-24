d6a39df309b074acd934f970b5a161ccce23bbfa
蔡孝玠 <asky.hunsin@gmail.com>
2018-08-24 18:37:28 +0800

    New status code

--- market/error.go
+++ market/error.go
@@ -19,16 +19,28 @@ func (e *Err) Status() pb.Status {
 	return e.s
 }
 
-// NotFound returns a pointer to Err with status
-// pb.NOT_FOUND and given message.
+// Cancelled returns a pointer to Err with status CANCELLED
+// and given message.
+func Cancelled(msg string) error {
+	return &Err{pb.Status_CANCELLED, msg}
+}
+
+// NotFound returns a pointer to Err with status NOT_FOUND
+// and given message.
 func NotFound(msg string) error {
 	return &Err{pb.Status_NOT_FOUND, msg}
 }
 
-// Unimplemented returns a pointer to Err with status
-// pb.UNIMPLEMENTED and given message.
+// Unimplemented returns a pointer to Err with status UNIMPLEMENTED
+// and given message.
 func Unimplemented(msg string) error {
 	return &Err{pb.Status_UNIMPLEMENTED, msg}
+}
+
+// Unlisted returns a pointer to Err with status UNLISTED
+// and given message.
+func Unlisted(msg string) error {
+	return &Err{pb.Status_UNLISTED, msg}
 }
 
 // Error returns a pointer to Err with given status code and message.
